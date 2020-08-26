2a04226687d2fbb04de071dc982e28f81e7e3954
Neelesh Shetty <an.neelesh@novasemita.org>
2020-08-27 00:13:23 +0530

    combineReducer

---
+++ index.js
@@ -0,0 +1,65 @@
+const redux = require('redux');
+const { createStore } = redux
+const { combineReducers } = redux
+
+const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
+
+function buyCake() {
+    return {
+        type: BUY_CAKE
+    }
+}
+
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM
+    }
+}
+
+// (previousState, action) => newState
+
+const initialCakeState = {
+    numOfCakes: 10
+}
+
+const initialIceCreamState = {
+    numOfIceCreams: 20
+}
+
+const cakeReducer = (state = initialCakeState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+
+        default: return state
+    }
+}
+
+const IceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
+        }
+
+        default: return state
+    }
+}
+
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: IceCreamReducer
+})
+
+const store = createStore(rootReducer)
+console.log('Initial state', store.getState())
+const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+unsubscribe()
