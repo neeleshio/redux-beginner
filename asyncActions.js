84fd645c743b19a99c435d9b4f486dce796b9fda
Neelesh Shetty <an.neelesh@novasemita.org>
2020-08-27 14:57:09 +0530

    api fetching & thunk middleware

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,7 @@
 const redux = require('redux')
-const { createStore } = redux
+const { createStore, applyMiddleware } = redux
+const thunkMiddleware = require('redux-thunk').default
+const axios = require('axios')
 
 const initialState = {
     loading: false,
@@ -19,14 +21,14 @@ const fetchUsersRequest = () => {
     }
 }
 
-const fetchUsersSuccess = () => {
+const fetchUsersSuccess = (users) => {
     return {
         type: FETCH_USERS_SUCCESS,
         payload: users
     }
 }
 
-const fetchUsersFailure = () => {
+const fetchUsersFailure = (error) => {
     return {
         type: FETCH_USERS_FAILURE,
         payload: error
@@ -57,4 +59,25 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-const store = createStore(reducer)
+//thunkMiddleware allows an actionCreator to return a function instead of an action.
+//so now the function can perform side effects such as asynchronous tasks
+
+const fetchUsers = () => {
+    return function (dispatch) {
+        dispatch(fetchUsersRequest())
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                const users = response.data.map(users => users.id)
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(error => {
+                dispatch(fetchUsersFailure(error.message))
+            })
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware))
+store.subscribe(() => console.log(store.getState()))
+store.dispatch(fetchUsers())
+
+/* END OF REDUX CONCEPTS */
